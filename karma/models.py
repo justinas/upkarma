88bc185c91caa6c2a17580221af172bfa55f9c55
Justinas Stankevičius <justinas@justinas.me>
2013-06-13 13:37:36 +0300

    Oh hey errors

--- karma/models.py
+++ karma/models.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
 
 class User(AbstractBaseUser):
-    screen_name = models.CharField('Twitter screen name', max_length=255)
+    screen_name = models.CharField('Twitter screen name', unique=True,
+                                   max_length=255)
     twitter_id = models.CharField('Twitter id', max_length=255)
-    avatar = models.URLField('Twitter avatar URL' blank=True)
+    avatar = models.URLField('Twitter avatar URL', blank=True)
     banned = models.BooleanField('Boolean showing if user is banned',
         default=0,
         null=False,
@@ -16,7 +17,7 @@ class User(AbstractBaseUser):
 
     # django auth thingies
     USERNAME_FIELD = 'screen_name'
-    REQUIRED_FIELDS = ['screen_name', 'twitter_id']
+    REQUIRED_FIELDS = ['twitter_id']
 
 class Tweet(models.Model):
     """
@@ -25,14 +26,13 @@ class Tweet(models.Model):
 
     sender = models.ForeignKey(
         User,
-        # why?
-        # null=True,
+        null=True,
         related_name='karma_sends',
         on_delete=models.SET_NULL
     )
     receiver = models.ForeignKey(
         User,
-        # null=True,
+        null=True,
         related_name='karma_receives',
         on_delete=models.SET_NULL
     )
