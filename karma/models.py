3a2905db96046e99143600442ad3730cc7a9ea1b
Justinas Stankevičius <justinas@justinas.me>
2013-06-13 13:34:01 +0300

    Base karma models

--- karma/models.py
+++ karma/models.py
@@ -1,9 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 
-class User(models.Model):
-    pass
+class User(AbstractBaseUser):
+    screen_name = models.CharField('Twitter screen name', max_length=255)
+    twitter_id = models.CharField('Twitter id', max_length=255)
+    avatar = models.URLField('Twitter avatar URL' blank=True)
+    banned = models.BooleanField('Boolean showing if user is banned',
+        default=0,
+        null=False,
+    )
+    # denormalized field for storing the point amount
+    points = models.IntegerField(default=0)
+
+    # django auth thingies
+    USERNAME_FIELD = 'screen_name'
+    REQUIRED_FIELDS = ['screen_name', 'twitter_id']
 
 class Tweet(models.Model):
-    pass
+    """
+    A Tweet, giving some karma points
+    """
+
+    sender = models.ForeignKey(
+        User,
+        # why?
+        # null=True,
+        related_name='karma_sends',
+        on_delete=models.SET_NULL
+    )
+    receiver = models.ForeignKey(
+        User,
+        # null=True,
+        related_name='karma_receives',
+        on_delete=models.SET_NULL
+    )
+    amount = models.IntegerField('Amount of points')
+    date = models.DateTimeField(auto_now_add=True)
+    twitter_id = models.CharField('Twitter id of the tweet', max_length=255)
+
+    text = models.TextField(blank=True)
