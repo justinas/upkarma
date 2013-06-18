333b9cb9bc9b8a1878baa68735949906e39b68f2
Justinas Stankevičius <justinas@justinas.me>
2013-06-18 17:42:32 +0300

    Updating utils

--- karma/utils.py
+++ karma/utils.py
@@ -1,5 +1,6 @@
-from twitter import Twitter, OAuth
+from twitter import Twitter, TwitterStream, OAuth
 from django.conf import settings
+from datetime import date, timedelta
 
 def get_global_client():
     """
@@ -9,3 +10,14 @@ def get_global_client():
     """
     t = Twitter(auth=OAuth(**settings.UPKARMA_SETTINGS['global_credentials']))
     return t
+
+def get_stream_client():
+    t = TwitterStream(auth=OAuth(**settings.UPKARMA_SETTINGS['global_credentials']))
+    return t
+
+def get_week_start(today=None):
+    if not today:
+        today = date.today()
+    # subtract the amount of days
+    # that has passed since Monday
+    return today - timedelta(days=today.weekday())
