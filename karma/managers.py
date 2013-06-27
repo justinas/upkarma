c8e8ab203755fe03c57f1db892c71cf397772cd3
Justinas Stankevičius <justinas@justinas.me>
2013-06-27 13:47:17 +0300

    Fixing Manager. That's what you get for not testing

--- karma/managers.py
+++ karma/managers.py
@@ -3,14 +3,14 @@ from django.db.models import Sum
 
 class UserManager(models.Manager):
     def with_points(self):
-        qs = self.get_queryset()
+        qs = self.get_query_set()
         return qs.annotate(points=Sum('karma_receives__amount'))
 
     def with_points_sent(self):
-        qs = self.get_queryset()
+        qs = self.get_query_set()
         return qs.annotate(points_sent=Sum('karma_sends__amount'))
 
 
     def top(self):
         qs = self.with_points()
-        return qs.order_by('-points')
+        return qs.order_by('-points').exclude(points=None)
