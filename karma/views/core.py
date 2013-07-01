bb0d0f67f1ed861653cfc261673bf77851cc74ee
Justinas Stankevičius <justinas@justinas.me>
2013-07-01 16:31:41 +0300

    User view now returns more info

--- karma/views/core.py
+++ karma/views/core.py
@@ -1,14 +1,28 @@
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
+from django.http import Http404
+from django.db.models import Sum
 
 from karma.models import User
+from karma.utils import cached
 
 def index(request):
     top = User.objects.top()[0:20]
 
     return render_to_response('karma/index.html', {'user_top' : top})
 
-def user(request, name):
-    user = get_object_or_404(User, screen_name__iexact=name)
+def get_full_user(name):
+    try:
+        qs = User.objects.exclude(banned=True)
+        user = qs.get(screen_name__iexact=name)
+        points = user.karma_receives.aggregate(s=Sum('amount'))['s']
+        points_sent = user.karma_sends.aggregate(s=Sum('amount'))['s']
 
-    return render_to_response('karma/user.html', {'user' : user})
+    except User.DoesNotExist:
+        raise Http404()
+
+    return dict(user=user, points=points, points_sent=points_sent)
+
+def user(request, name):
+    context = get_full_user(name)
+    return render_to_response('karma/user.html', context)
