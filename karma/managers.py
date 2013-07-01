472ac56b42d56b7febc62c304da98ae941ab5a01
Justinas Stankevičius <justinas@justinas.me>
2013-07-01 16:16:37 +0300

    Revert "Oh wow refactoring a custom Manager into custom Manager + QuerySet"
    This reverts commit 991d5a3d34307971d0d51e5d14540475af2d1b10.
    That was useless and didn't work right

--- karma/managers.py
+++ karma/managers.py
@@ -1,19 +1,15 @@
 from django.db import models
 from django.db.models import Sum
-from django.db.models.query import QuerySet
-
-class UserQuerySet(QuerySet):
-    def with_points(self):
-        return self.annotate(points=Sum('karma_receives__amount'))
-
-    def with_points_sent(self):
-        return self.annotate(points_sent=Sum('karma_sends__amount'))
 
 class UserManager(models.Manager):
-    def get_query_set(self):
-        return UserQuerySet(self.model)
+    def with_points(self):
+        qs = self.get_query_set()
+        return qs.annotate(points=Sum('karma_receives__amount'))
+
+    def with_points_sent(self):
+        qs = self.get_query_set()
+        return qs.annotate(points_sent=Sum('karma_sends__amount'))
 
     def top(self):
-        qs = self.get_query_set()
-        qs = qs.with_points().exclude(banned=True)
+        qs = self.with_points().exclude(banned=True)
         return qs.order_by('-points').exclude(points=None)
