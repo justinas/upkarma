dd1a3c0c5851c40dbe3e4ab45ccf5dbfd838cabc
Justinas Stankevičius <justinas@justinas.me>
2013-07-21 18:09:15 +0300

    Rest Framework config

--- upkarma_project/settings.py
+++ upkarma_project/settings.py
@@ -76,12 +76,28 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+
     'south',
+    'rest_framework',
+
     'karma',
     'karma.importer',
     'karma.bot',
     'karma.stream',
 )
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 TEST_RUNNER = 'karma.test_runner.KarmaTestRunner'
 
