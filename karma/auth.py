60ca207970dac169089a58a4a52afde366bc97aa
Justinas Stankevičius <justinas@justinas.me>
2013-08-04 21:16:30 +0300

    fixing faulty auth implementation

--- karma/auth.py
+++ karma/auth.py
@@ -7,10 +7,13 @@ class TwitterBackend(object):
     """
 
     def authenticate(self, twitter_id=None):
-        return self.get_user(twitter_id)
-
-    def get_user(self, twitter_id=None):
         try:
             return User.objects.get(twitter_id=twitter_id)
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, pk=None):
+        try:
+            return User.objects.get(pk=pk)
         except User.DoesNotExist:
             return None
