136804a6681bbbabcab919c8fa122ca3dfe55d1c
Justinas Stankevičius <justinas@justinas.me>
2013-08-07 18:12:51 +0300

    Markdown support

--- karma/news/models.py
+++ karma/news/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.conf import settings
 
+import markdown
+
 # text formats
 HTML = 0
 MARKDOWN = 1
+
+# default to markdown
+FORMAT = getattr(settings, 'KARMA_NEWS_FORMAT', 1)
 
 class Entry(models.Model):
     title = models.CharField(max_length=255, blank=False)
@@ -14,3 +19,11 @@ class Entry(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     date = models.DateTimeField()
     published = models.BooleanField(default=False)
+
+    def save(self, **kwargs):
+        if FORMAT == HTML:
+            pass # do nothing, it
+        elif FORMAT == MARKDOWN:
+            self.text_rendered = markdown.markdown(self.text)
+
+        super(Entry, self).save()
