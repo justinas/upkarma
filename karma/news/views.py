c448a37e4661f9b20523f0c6303cd3b93991ac64
Justinas Stankevičius <justinas@justinas.me>
2013-08-08 12:32:49 +0300

    news_list

--- karma/news/views.py
+++ karma/news/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.shortcuts import render
+from .models import Entry
+
+def news_list(request):
+    entries = Entry.public.all()
+
+    context = dict(entries=entries)
+
+    return render(request, 'karma/news_list.html', context)
