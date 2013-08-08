4a991322f1b13821e614f16a1d6036e7598a19bf
Justinas Stankevičius <justinas@justinas.me>
2013-08-08 12:32:38 +0300

    News list url

--- karma/urls.py
+++ karma/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url, include
 from rest_framework.routers import DefaultRouter
 
 from . import views
+from karma.news import views as news_views
 
 api_router = DefaultRouter()
 api_router.register(r'users', views.api.UserViewSet)
@@ -18,4 +19,6 @@ urlpatterns = patterns(
     url(r'^auth/login/$', views.auth.start_login),
     url(r'^auth/callback/$', views.auth.callback),
     url(r'^auth/logout/$', views.auth.logout_view),
+
+    url(r'^news/$', news_views.news_list),
 )
