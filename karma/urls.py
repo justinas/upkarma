1827e8a5b2cbc9655a771525104066c5242833ed
Justinas Stankevicius <justinas@justinas.org>
2017-12-03 14:24:09 +0200

    Migrate to Django 1.10

--- karma/urls.py
+++ karma/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -9,18 +9,18 @@ api_router.register(r'users', views.api.UserViewSet)
 api_router.register(r'tweets', views.api.TweetViewSet)
 
 urlpatterns = [
-    url(r'^$', views.index, name='karma.views.index'),
-    url(r'^user/(.+)/$', views.user, name='karma.views.user'),
-    url(r'^search/$', views.search, name='karma.views.search'),
-    url(r'^stats/$', views.stats, name='karma.views.stats'),
+    url(r'^$', views.index, name='index'),
+    url(r'^user/(.+)/$', views.user, name='user'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^stats/$', views.stats, name='stats'),
     url(r'^api/', include(api_router.urls)),
 
-    url(r'^auth/login/$', views.auth.start_login),
-    url(r'^auth/callback/$', views.auth.callback),
-    url(r'^auth/logout/$', views.auth.logout_view),
-    url(r'^auth/no-user/$', views.auth.no_user),
+    url(r'^auth/login/$', views.auth.start_login, name='start-login'),
+    url(r'^auth/callback/$', views.auth.callback, name='auth-callback'),
+    url(r'^auth/logout/$', views.auth.logout_view, name='logout-view'),
+    url(r'^auth/no-user/$', views.auth.no_user, name='no-user'),
 
-    url(r'^news/$', news_views.news_list),
-    url(r'^news/(\d+)/$', news_views.news_single),
-    url(r'^guide/$', views.guide, name='karma.views.guide'),
+    url(r'^news/$', news_views.news_list, name='news-list'),
+    url(r'^news/(\d+)/$', news_views.news_single, name='news-single'),
+    url(r'^guide/$', views.guide, name='guide'),
 ]
