eb3948e8d79059ed3893e99edf2bb7fa4aa75adb
Justinas Stankevicius <justinas@justinas.org>
2018-12-29 15:34:10 +0200

    Just use set -e for errors

--- design.sh
+++ design.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 KD="$DIR/karma_design"
 
@@ -6,15 +8,15 @@ echo "Building CSS"
 if [ "$1" == "full" ]
 then
     # things that rarely change
-    (lessc $KD/less/bootstrap.less > $KD/static/bootstrap.css && \
-    lessc $KD/less/responsive.less > $KD/static/bootstrap-responsive.css && \
-    lessc $KD/less/styler.less > $KD/static/styler.css) || echo "failure!" && exit 1
-    (yui-compressor $KD/static/bootstrap.css > $KD/static/bootstrap.min.css && \
-    yui-compressor $KD/static/bootstrap-responsive.css > $KD/static/bootstrap-responsive.min.css) \
-    || echo "failure!" && exit 1
+    lessc $KD/less/bootstrap.less > $KD/static/bootstrap.css
+    lessc $KD/less/responsive.less > $KD/static/bootstrap-responsive.css
+    lessc $KD/less/styler.less > $KD/static/styler.css
+
+    yui-compressor $KD/static/bootstrap.css > $KD/static/bootstrap.min.css
+    yui-compressor $KD/static/bootstrap-responsive.css > $KD/static/bootstrap-responsive.min.css
 fi
 
-lessc $KD/less/upkarma.less > $KD/static/upkarma.css || (echo "failure!" && exit 1)
+lessc $KD/less/upkarma.less > $KD/static/upkarma.css
 
 
 echo "Collecting static"
