be9b2ac4a38c98169de9c89643170fefffe8fd00
Justinas Stankevicius <justinas@justinas.org>
2018-12-29 15:31:23 +0200

    Fix failure exits

--- design.sh
+++ design.sh
@@ -8,10 +8,10 @@ then
     # things that rarely change
     (lessc $KD/less/bootstrap.less > $KD/static/bootstrap.css && \
     lessc $KD/less/responsive.less > $KD/static/bootstrap-responsive.css && \
-    lessc $KD/less/styler.less > $KD/static/styler.css) || (echo "failure!" && exit 1)
-    yuicompressor $KD/static/bootstrap.css > $KD/static/bootstrap.min.css
-    yuicompressor $KD/static/bootstrap-responsive.css > \
-        $KD/static/bootstrap-responsive.min.css
+    lessc $KD/less/styler.less > $KD/static/styler.css) || echo "failure!" && exit 1
+    (yui-compressor $KD/static/bootstrap.css > $KD/static/bootstrap.min.css && \
+    yui-compressor $KD/static/bootstrap-responsive.css > $KD/static/bootstrap-responsive.min.css) \
+    || echo "failure!" && exit 1
 fi
 
 lessc $KD/less/upkarma.less > $KD/static/upkarma.css || (echo "failure!" && exit 1)
